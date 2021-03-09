27587cf7e5faf8d174587d0ffb7914230e8f62fc
yiwei.li <yiwei.li@graviti.com>
2021-03-09 12:12:19 +0800

    test(geometry): add missing unit test for vector.py
    add test for bool, neg, eq, _process_args, _repr_head in TestVector.
    add TestVector2D and TestVector3D.

--- tensorbay/geometry/tests/test_vector.py
+++ tensorbay/geometry/tests/test_vector.py
@@ -3,8 +3,6 @@
 # Copyright 2021 Graviti. Licensed under MIT License.
 #
 
-"""This file defines class TestVector"""
-
 import pytest
 
 from .. import Vector, Vector2D, Vector3D
@@ -13,25 +11,95 @@ _DATA = [{"x": 1.0, "y": 2.0}, {"x": 1.0, "y": 2.0, "z": 3.0}]
 
 
 class TestVector:
-    """Test Vector class."""
-
-    def test_new_class(self) -> None:
+    def test_new_class(self):
         with pytest.raises(TypeError):
             Vector(1)
         with pytest.raises(ValueError):
             Vector(1, 2, 3, 4)
         assert Vector(1, 2) == Vector2D(1, 2)
         assert Vector(1, 2, 3) == Vector3D(1, 2, 3)
+        assert Vector([1, 2]) == Vector2D(1, 2)
 
-    @pytest.mark.parametrize("loads", _DATA)
-    def test_loads_and_dumps(self, loads) -> None:
-        vector = Vector.loads(loads)
-        assert vector.dumps() == loads
+    def test_bool(self):
+        vector = Vector(0, 0)
+        assert bool(vector) == False
 
-    def test_add(self) -> None:
+        vector = Vector(1, 1)
+        assert bool(vector) == True
+
+    def test_neg(self):
+        vector = Vector(1, 1)
+        assert -vector == Vector2D(-1, -1)
+
+    def test_eq(self):
+        vector_1 = Vector(1, 2)
+        vector_2 = Vector(1, 2)
+        vector_3 = Vector(1, 3)
+        vector_4 = Vector(1, 2, 3)
+        assert (vector_1 == vector_2) == True
+        assert (vector_1 == vector_3) == False
+        assert (vector_1 == vector_4) == False
+
+    def test_add(self):
         vector_2d = Vector(1, 2)
         vector_3d = Vector(1, 2, 3)
         with pytest.raises(ValueError):
             vector_2d + vector_3d
+        with pytest.raises(TypeError):
+            vector_2d + 1
         assert Vector(1, 1) + Vector(1, 1) == Vector(2, 2)
         assert Vector(1, 1, 1) + Vector(1, 1, 1) == Vector(2, 2, 2)
+
+    def test_radd(self):
+        vector = Vector(1, 2)
+        assert [1, 1] + vector == Vector(2, 3)
+
+    def test_process_args(self):
+        with pytest.raises(TypeError):
+            Vector._process_args(1)
+        assert Vector._process_args(1, 2) == (1, 2)
+        assert Vector._process_args([1, 2]) == (1, 2)
+
+    def test_repr_head(self):
+        vector = Vector(1, 2)
+        assert vector._repr_head() == "Vector2D(1, 2)"
+
+    @pytest.mark.parametrize("loads", _DATA)
+    def test_loads_and_dumps(self, loads):
+        vector = Vector.loads(loads)
+        assert vector.dumps() == loads
+
+
+class TestVector2D:
+    def test_new(self):
+        with pytest.raises(TypeError):
+            Vector2D(1, 2, 3)
+        assert Vector2D(None) == Vector2D(0.0, 0.0)
+        assert Vector2D([1, 2]) == Vector2D(1, 2)
+        assert Vector2D(x=1, y=1) == Vector2D(1, 1)
+
+        vector_2d = Vector2D(1, 2)
+        assert vector_2d.x == 1
+        assert vector_2d.y == 2
+
+    def test_loads_and_dumps(self):
+        vector_2d = Vector2D.loads(_DATA[0])
+        assert vector_2d.dumps() == _DATA[0]
+
+
+class TestVector3D:
+    def test_new(self):
+        with pytest.raises(TypeError):
+            Vector3D(1, 2)
+        assert Vector3D(None) == Vector3D(0.0, 0.0, 0.0)
+        assert Vector3D([1, 2, 3]) == Vector3D(1, 2, 3)
+        assert Vector3D(x=1, y=2, z=3) == Vector3D(1, 2, 3)
+
+        vector_3d = Vector3D(1, 2, 3)
+        assert vector_3d.x == 1
+        assert vector_3d.y == 2
+        assert vector_3d.z == 3
+
+    def test_loads_and_dumps(self):
+        vector_3d = Vector3D.loads(_DATA[1])
+        assert vector_3d.dumps() == _DATA[1]
