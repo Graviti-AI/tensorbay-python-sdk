c20c200a780ea9b2f32d0449de7ebdb8086068bf
yiwei.li <yiwei.li@graviti.com>
2021-03-09 14:54:15 +0800

    test(geometry): remove meaningless assertion in TestBox2D.test_init

--- tensorbay/geometry/tests/test_box.py
+++ tensorbay/geometry/tests/test_box.py
@@ -22,7 +22,6 @@ class TestBox2D:
         with pytest.raises(TypeError):
             Box2D(x=0)
         assert Box2D(None) == Box2D(0.0, 0.0, 0.0, 0.0)
-        assert Box2D(1, 2, 3, 4) == Box2D(1, 2, 3, 4)
         assert Box2D([1, 2, 3, 4]) == Box2D(1, 2, 3, 4)
         assert Box2D(x=1, y=2, width=3, height=4) == Box2D(1, 2, 4, 6)
 
