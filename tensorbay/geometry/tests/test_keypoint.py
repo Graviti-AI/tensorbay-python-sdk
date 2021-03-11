e1338e974e6646b8ccbeab00b89a26baf6856ffa
yiwei.li <yiwei.li@graviti.com>
2021-03-11 10:54:04 +0800

    test(geometry): add test_eq, test_dumps, test_bounds to TestKeypoints2D

--- tensorbay/geometry/tests/test_keypoint.py
+++ tensorbay/geometry/tests/test_keypoint.py
@@ -5,7 +5,7 @@
 
 import pytest
 
-from .. import Keypoint2D, Keypoints2D, Vector2D
+from .. import Box2D, Keypoint2D, Keypoints2D, Vector2D
 
 _DATA_KEYPOINT = {"x": 1.0, "y": 1.0, "v": 1}
 _DATA_KEYPOINTS = [{"x": 1.0, "y": 1.0, "v": 1}, {"x": 2.0, "y": 2.0, "v": 2}]
@@ -40,6 +40,21 @@ class TestKeypoints2D:
         assert Keypoints2D(None) == Keypoints2D([])
         assert Keypoints2D(sequence) == Keypoints2D([Keypoint2D(1, 2), Keypoint2D(2, 3)])
 
+    def test_eq(self):
+        keypoints_1 = Keypoints2D([[1, 2], [2, 3]])
+        keypoints_2 = Keypoints2D([[1, 2], [2, 3]])
+        keypoints_3 = Keypoints2D([[1, 2], [3, 3]])
+        assert (keypoints_1 == keypoints_2) == True
+        assert (keypoints_1 == keypoints_3) == False
+
     def test_loads(self):
         keypoints = Keypoints2D.loads(_DATA_KEYPOINTS)
         assert keypoints._data == [Keypoint2D(1.0, 1.0, 1), Keypoint2D(2.0, 2.0, 2)]
+
+    def test_dumps(self):
+        keypoints = Keypoints2D([[1, 1, 1], [2, 2, 2]])
+        assert keypoints.dumps() == _DATA_KEYPOINTS
+
+    def test_bounds(self):
+        keypoints = Keypoints2D([[1, 2], [2, 3]])
+        assert keypoints.bounds() == Box2D(1, 2, 2, 3)
