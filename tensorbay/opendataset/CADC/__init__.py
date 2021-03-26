a649e7c2d5f18ab408e3f92480dac676947f8e76
yexuan.li <yexuan.li@graviti.com>
2021-03-26 15:40:37 +0800

    feat(opendataset): add loader for CADC opendataset

---
+++ tensorbay/opendataset/CADC/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name
+
+"""Dataloader of the CADC dataset."""
+
+from .loader import CADC
+
+__all__ = ["CADC"]
