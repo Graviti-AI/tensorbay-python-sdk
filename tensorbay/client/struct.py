563d2368821df401e45a738a51b507da14b71e07
yexuan.li <yexuan.li@graviti.com>
2021-03-31 19:37:31 +0800

    feat(client): add Tag class and Branch class

--- tensorbay/client/struct.py
+++ tensorbay/client/struct.py
@@ -3,9 +3,19 @@
 # Copyright 2021 Graviti. Licensed under MIT License.
 #
 
-"""Commit."""
+"""User, Commit, Tag and Branch classes.
 
-from typing import Any, Dict, Type, TypeVar
+:class:`User` defines the basic concept of a user with an action.
+
+:class:`Commit` defines the structure of a commit.
+
+:class:`Tag` defines the structure of a commit tag.
+
+:class:`Branch` defines the structure of a branch.
+
+"""
+
+from typing import Any, Dict, Optional, Tuple, Type, TypeVar
 
 from ..utility import EqMixin, ReprMixin, common_loads
 
@@ -80,13 +90,13 @@ class Commit(ReprMixin, EqMixin):
 
     _T = TypeVar("_T", bound="Commit")
 
-    _repr_attrs = ("commit_id", "parent_commit_id", "message", "committer")
+    _repr_attrs: Tuple[str, ...] = ("commit_id", "parent_commit_id", "message", "committer")
     _repr_maxlevel = 2
 
     def __init__(
         self,
         commit_id: str,
-        parent_commit_id: str,
+        parent_commit_id: Optional[str],
         message: str,
         committer: User,
     ) -> None:
@@ -147,3 +157,110 @@ class Commit(ReprMixin, EqMixin):
             "message": self.message,
             "committer": self.committer.dumps(),
         }
+
+
+class _NamedCommit(Commit):
+    """This class defines the structure of a named commit.
+
+    :class:`_NamedCommit` is the base class of :class:`Tag` and :class:`Branch`.
+
+    Arguments:
+        name: The name of the named commit.
+        commit_id: The commit id.
+        parent_commit_id: The parent commit id.
+        message: The commit message.
+        committer: The commit user.
+
+    """
+
+    _T = TypeVar("_T", bound="_NamedCommit")
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        name: str,
+        commit_id: str,
+        parent_commit_id: Optional[str],
+        message: str,
+        committer: User,
+    ) -> None:
+        super().__init__(commit_id, parent_commit_id, message, committer)
+        self.name = name
+
+    def _repr_head(self) -> str:
+        return f'{self.__class__.__name__}("{self.name}")'
+
+    def _loads(self, contents: Dict[str, Any]) -> None:
+        super()._loads(contents)
+        self.name = contents["name"]
+
+    @classmethod
+    def loads(cls: Type[_T], contents: Dict[str, Any]) -> _T:
+        """Loads a :class:`_NamedCommit` instance for the given contents.
+
+        Arguments:
+            contents: A dict containing all the information of the named commit::
+
+                    {
+                        "name": <str>
+                        "commitId": <str>
+                        "parentCommitId": <str> or None
+                        "message": <str>
+                        "committer": {
+                            "name": <str>
+                            "date": <int>
+                        }
+                    }
+
+        Returns:
+            A :class:`_NamedCommit` instance containing all the information in the given contents.
+
+        """
+        return common_loads(cls, contents)
+
+    def dumps(self) -> Dict[str, Any]:
+        """Dumps all the named commit information into a dict.
+
+        Returns:
+            A dict containing all the information of the named commit::
+
+                {
+                    "name": <str>
+                    "commitId": <str>
+                    "parentCommitId": <str> or None
+                    "message": <str>
+                    "committer": {
+                        "name": <str>
+                        "date": <int>
+                    }
+                }
+
+        """
+        contents = super().dumps()
+        contents["name"] = self.name
+        return contents
+
+
+class Tag(_NamedCommit):
+    """This class defines the structure of the tag of a commit.
+
+    Arguments:
+        name: The name of the tag.
+        commit_id: The commit id.
+        parent_commit_id: The parent commit id.
+        message: The commit message.
+        committer: The commit user.
+
+    """
+
+
+class Branch(_NamedCommit):
+    """This class defines the structure of a branch.
+
+    Arguments:
+        name: The name of the branch.
+        commit_id: The commit id.
+        parent_commit_id: The parent commit id.
+        message: The commit message.
+        committer: The commit user.
+
+    """
