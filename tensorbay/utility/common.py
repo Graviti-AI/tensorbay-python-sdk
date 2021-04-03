4e5c95f39f37c9d0e98ba9c7f906d2ea243e67de
linjiX <linji.xue@graviti.cn>
2021-04-03 22:40:28 +0800

    feat(utility): implement decorator "Deprecated" for deprecated functions
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/336

--- tensorbay/utility/common.py
+++ tensorbay/utility/common.py
@@ -12,7 +12,9 @@ which compares all the instance variables.
 
 """
 
-from typing import Any, Sequence, Type, TypeVar, Union
+import warnings
+from functools import wraps
+from typing import Any, Callable, Optional, Sequence, Type, TypeVar, Union
 
 import numpy as np
 
@@ -48,3 +50,75 @@ class EqMixin:  # pylint: disable=too-few-public-methods
             return False
 
         return self.__dict__ == other.__dict__
+
+
+_Callable = TypeVar("_Callable", bound=Callable[..., Any])
+
+
+class Deprecated:  # pylint: disable=too-few-public-methods
+    """A decorator for deprecated functions.
+
+    Arguments:
+        remove_in: The version the function will be removed in.
+        substitute: The substitute function.
+
+    """
+
+    def __init__(
+        self, *, since: str, removed_in: Optional[str] = None, substitute: Optional[str] = None
+    ) -> None:
+        self._since = since
+        self._removed_in = removed_in
+        self._substitute = substitute
+
+    def __call__(self, func: _Callable) -> _Callable:
+        """Wrap the decorated function by adding the deprecated message.
+
+        Arguments:
+            func: The deprecated function.
+
+        Returns:
+            The wrapped function which shows the deprecated message when calling.
+
+        """
+        messages = [f'Function "{func.__name__}" is deprecated since version {self._since}.']
+        if self._removed_in:
+            messages.append(f'It will be removed in version "{self._removed_in}".')
+
+        if self._substitute:
+            messages.append(f'Use "{self._substitute}" instead.')
+
+        message = " ".join(messages)
+
+        @wraps(func)
+        def wrapper(*arg: Any, **kwargs: Any) -> Any:
+            warnings.warn(message, DeprecationWarning, 2)
+
+            return func(*arg, **kwargs)
+
+        wrapper.__doc__ = self._update_docstring(wrapper.__doc__)
+
+        return wrapper  # type: ignore[return-value]
+
+    def _update_docstring(self, docstring: Optional[str]) -> str:
+        insert_block = [f".. deprecated:: {self._since}"]
+        if self._removed_in:
+            insert_block.append(f"   Will be removed in version {self._removed_in}.")
+        if self._substitute:
+            insert_block.append(f"   Use :meth:`{self._substitute}` instead.")
+
+        if not docstring:
+            return "\n".join(insert_block)
+
+        lines = docstring.splitlines()
+
+        indent = ""
+        for line in lines[1:]:
+            if line:
+                indent = line[: -len(line.lstrip())]
+                break
+
+        lines[1:1] = (f"{indent}{message}" for message in insert_block)
+        lines.insert(1, "")
+
+        return "\n".join(lines)
