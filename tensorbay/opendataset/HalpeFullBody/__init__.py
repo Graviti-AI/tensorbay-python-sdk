4aa5867307c9bceaf4e3cc7c86fcf34cee8a8f52
yiwei.li <yiwei.li@graviti.com>
2021-04-08 16:17:34 +0800

    feat(opendataset): add loader for HalpeFullBody opendataset
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/364

---
+++ tensorbay/opendataset/HalpeFullBody/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name
+
+"""Dataloader of the Halpe Full-Body Human Keypoints and HOI-Det dataset."""
+
+from .loader import HalpeFullBody
+
+__all__ = ["HalpeFullBody"]
---
+++ tensorbay/opendataset/HalpeFullBody/loader.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name
+
+"""Dataloader of the Halpe Full-Body Human Keypoints and HOI-Det dataset."""
+
+import json
+import os
+from typing import Any, Dict
+
+from ...dataset import Data, Dataset
+from ...geometry import Keypoint2D
+from ...label import Classification, LabeledBox2D, LabeledKeypoints2D
+
+DATASET_NAME = "Halpe Full-Body Human Keypoints and HOI-Det"
+
+_SEGMENT_SPLIT = (
+    ("train", "halpe_train_v1.json", os.path.join("hico_20160224_det", "images", "train2015")),
+    ("val", "halpe_val_v1.json", "val2017"),
+)
+
+
+def HalpeFullBody(path: str) -> Dataset:
+    """Dataloader of the Halpe Full-Body Human Keypoints and HOI-Det dataset.
+
+    Arguments:
+        path: The root directory of the dataset.
+            The folder structure should be like::
+
+                <path>
+                    halpe_train_v1.json
+                    halpe_val_v1.json
+                    hico_20160224_det/
+                        images/
+                            train2015/
+                                HICO_train2015_00000001.jpg
+                                ...
+                    val2017/
+                        000000000139.jpg
+                        ...
+
+    Returns:
+        Loaded `Dataset` object.
+
+    """
+    root_path = os.path.abspath(os.path.expanduser(path))
+
+    dataset = Dataset(DATASET_NAME)
+    dataset.load_catalog(os.path.join(os.path.dirname(__file__), "catalog.json"))
+
+    for mode, label_file, image_directory in _SEGMENT_SPLIT:
+        segment = dataset.create_segment(mode)
+
+        with open(os.path.join(root_path, label_file), "r", encoding="utf-8") as fp:
+            annotations = json.load(fp)
+
+        if mode == "train":
+            for annotation, image in zip(annotations["annotations"], annotations["images"]):
+                image_path = os.path.join(root_path, image_directory, image["file_name"])
+                segment.append(_get_data(image_path, annotation))
+        else:
+            for annotation in annotations["annotations"]:
+                image_path = os.path.join(
+                    root_path, image_directory, f"{annotation['image_id']:012}.jpg"
+                )
+                segment.append(_get_data(image_path, annotation))
+
+    return dataset
+
+
+def _get_data(image_path: str, annotation: Dict[str, Any]) -> Data:
+    data = Data(image_path)
+
+    keypoints_iter = iter(annotation["keypoints"])
+    keypoints = LabeledKeypoints2D()
+    for x, y, v in zip(keypoints_iter, keypoints_iter, keypoints_iter):
+        keypoints.append(Keypoint2D(x, y, v if v in (0, 1, 2) else 2))  # pylint: disable=no-member
+
+    data.label.keypoints2d = [keypoints]
+
+    data.label.box2d = [LabeledBox2D.from_xywh(*annotation["bbox"])]
+
+    if "hoi" in annotation.keys():
+        data.label.classification = Classification(attributes={"hoi": annotation["hoi"]})
+
+    return data
