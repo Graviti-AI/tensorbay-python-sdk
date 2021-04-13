ad4649affce0de6abc2f0f346ab47715670c115a
yexuan.li <yexuan.li@graviti.com>
2021-04-13 14:10:19 +0800

    feat(exception): add TensorBayException base class for custom exceptions
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/380

---
+++ tensorbay/exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+
+"""TensorBayException."""
+
+
+class TensorBayException(Exception):
+    """This is the base class for TensorBay custom exceptions."""
