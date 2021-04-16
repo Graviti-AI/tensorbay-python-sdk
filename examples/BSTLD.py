dbc1f37dd2f2d88f625bc6584148be4956ca20f0
yiwei.li <yiwei.li@graviti.com>
2021-04-16 14:07:53 +0800

    docs: refine examples/BSTLD.rst
    Refined contents:
    - delete "List Dataset Names" part
    - add step number to "Organize Dataset"
    - add code of regular import in writing dataloader
    - add use of available dataloaders
    - polish the description, reduce meaningless sentences
    
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/404

--- examples/BSTLD.py
+++ examples/BSTLD.py
@@ -10,6 +10,8 @@
 # pylint: disable=import-error
 # pylint: disable=pointless-string-statement
 # pylint: disable=invalid-name
+# pylint: disable=unused-import
+# flake8: noqa: F401
 
 """This file includes the python code of BSTLD.rst and read_dataset_class.rst."""
 
@@ -24,19 +26,20 @@ gas = GAS(ACCESS_KEY)
 gas.create_dataset("BSTLD")
 """"""
 
-"""List Dataset Names"""
-gas.list_dataset_names()
+"""Organize Dataset / regular import"""
+from tensorbay.dataset import Data, Dataset
+from tensorbay.label import LabeledBox2D
+
 """"""
 
-"""Read Dataset Class / organize dataset"""
+"""Organize dataset / import dataloader"""
 from tensorbay.opendataset import BSTLD
 
 dataset = BSTLD("path/to/dataset/directory")
 """"""
 
 """Upload Dataset"""
-# dataset is the one you initialized in "Organize Dataset" section
-dataset_client = gas.upload_dataset(dataset, jobs=8, skip_uploaded_files=False)
+dataset_client = gas.upload_dataset(dataset)
 dataset_client.commit("initial commit")
 """"""
 
