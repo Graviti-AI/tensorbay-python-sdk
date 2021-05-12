295634ed2abb6a1dfbbcbbedc27e51108a4400a0
pengfei.wu <pengfei.wu@graviti.com>
2021-05-12 20:31:59 +0800

    feat(cli): support deleting dataset in "gas dataset" command
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/530

--- tensorbay/cli/dataset.py
+++ tensorbay/cli/dataset.py
@@ -14,13 +14,34 @@ from ..utility import TBRN, TBRNType
 from .utility import get_gas
 
 
-def _implement_dataset(obj: Dict[str, str], tbrn: str) -> None:
+def _implement_dataset(obj: Dict[str, str], tbrn: str, is_delete: bool, yes: bool) -> None:
     gas = get_gas(**obj)
+    if is_delete:
+        if not tbrn:
+            click.echo("Missing argument TBRN", err=True)
+            sys.exit(1)
+
+        info = TBRN(tbrn=tbrn)
+        if info.type != TBRNType.DATASET:
+            click.echo(f'"{tbrn}" is not a dataset', err=True)
+            sys.exit(1)
+
+        if not yes:
+            click.confirm(
+                f'Dataset "{tbrn}" will be completely deleted.\nDo you want to continue?',
+                abort=True,
+            )
+
+        gas.delete_dataset(info.dataset_name)
+        click.echo(f'Dataset "{tbrn}" is deleted successfully')
+        return
+
     if tbrn:
         info = TBRN(tbrn=tbrn)
         if info.type != TBRNType.DATASET:
             click.echo(f'"{tbrn}" is not a dataset', err=True)
             sys.exit(1)
+
         gas.create_dataset(info.dataset_name)
         click.echo(f'Dataset "{tbrn}" is created successfully')
     else:
