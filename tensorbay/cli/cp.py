75544c3e09c4ddc30be3851243b1d07f966140d0
rex.zheng <rex.zheng@graviti.com>
2021-05-17 11:03:42 +0800

    feat(cli): support uploading files in "gas cp" command
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/551

---
+++ tensorbay/cli/cp.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+
+"""Implementation of gas cp."""
+
+import os
+import sys
+from pathlib import Path, PurePosixPath
+from typing import Dict, Iterable
+
+import click
+
+from ..dataset import Data, Segment
+from .tbrn import TBRN, TBRNType
+from .utility import get_dataset_client, get_gas
+
+
+def _implement_cp(  # pylint: disable=too-many-arguments
+    obj: Dict[str, str],
+    local_paths: Iterable[str],
+    tbrn: str,
+    is_recursive: bool,
+    jobs: int,
+    skip_uploaded_files: bool,
+) -> None:
+    gas = get_gas(**obj)
+    info = TBRN(tbrn=tbrn)
+
+    dataset_client = get_dataset_client(gas, info, is_fusion=False)
+
+    if info.type not in (TBRNType.SEGMENT, TBRNType.NORMAL_FILE):
+        click.echo(f'"{tbrn}" is not a segment or file type', err=True)
+        sys.exit(1)
+
+    target_remote_path = info.remote_path if info.type == TBRNType.NORMAL_FILE else ""
+
+    local_abspaths = [os.path.abspath(local_path) for local_path in local_paths]
+    if (
+        len(local_abspaths) == 1
+        and not os.path.isdir(local_abspaths[0])
+        and target_remote_path
+        and not target_remote_path.endswith("/")
+    ):
+        segment_client = dataset_client.get_or_create_segment(info.segment_name)
+        segment_client.upload_file(local_abspaths[0], target_remote_path)
+    else:
+        segment = _get_segment(info.segment_name, local_abspaths, target_remote_path, is_recursive)
+        dataset_client.upload_segment(segment, jobs=jobs, skip_uploaded_files=skip_uploaded_files)
+
+
+def _get_segment(
+    segment_name: str,
+    local_abspaths: Iterable[str],
+    remote_path: str,
+    is_recursive: bool,
+) -> Segment:
+    """Get the pair of local_path and remote_path.
+
+    Arguments:
+        segment_name: The name of the segment these data belong to.
+        local_abspaths: A list of local abstract paths, could be folder or file.
+        remote_path: The remote object path, not necessarily end with '/'.
+        is_recursive: Whether copy directories recursively.
+
+    Returns:
+        A segment contains mapping data.
+
+    """
+    segment = Segment(segment_name)
+    for local_abspath in local_abspaths:
+        if not os.path.isdir(local_abspath):
+            data = Data(
+                local_abspath,
+                target_remote_path=str(PurePosixPath(remote_path, os.path.basename(local_abspath))),
+            )
+            segment.append(data)
+            continue
+
+        if not is_recursive:
+            click.echo(
+                "Local paths include directories, please use -r option",
+                err=True,
+            )
+            sys.exit(1)
+
+        local_abspath = os.path.normpath(local_abspath)
+        folder_name = os.path.basename(local_abspath)
+        for root, _, filenames in os.walk(local_abspath):
+            relpath = os.path.relpath(root, local_abspath) if root != local_abspath else ""
+            for filename in filenames:
+                data = Data(
+                    os.path.join(root, filename),
+                    target_remote_path=str(
+                        PurePosixPath(Path(remote_path, folder_name, relpath, filename))
+                    ),
+                )
+                segment.append(data)
+    return segment
