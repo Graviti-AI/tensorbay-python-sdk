7767227e11cf99dd9128acc88f569e882cc504d3
linji.xue <linji.xue@graviti.com>
2021-05-19 17:08:30 +0800

    refactor(cli): use TBRN instead of name to create dataset by gas dataset
    Basically this reverts commit 088736546f2a294742a4bd8c18614aa9d708a2b6.
    
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/571

--- tensorbay/cli/dataset.py
+++ tensorbay/cli/dataset.py
@@ -14,35 +14,36 @@ from .tbrn import TBRN, TBRNType
 from .utility import get_gas
 
 
-def _implement_dataset(obj: Dict[str, str], name: str, is_delete: bool, yes: bool) -> None:
+def _implement_dataset(obj: Dict[str, str], tbrn: str, is_delete: bool, yes: bool) -> None:
     gas = get_gas(**obj)
     if is_delete:
-        if not name:
+        if not tbrn:
             click.echo("Missing argument TBRN", err=True)
             sys.exit(1)
 
-        info = TBRN(tbrn=name)
+        info = TBRN(tbrn=tbrn)
         if info.type != TBRNType.DATASET:
-            click.echo(f'"{name}" is not a dataset', err=True)
+            click.echo(f'"{tbrn}" is not a dataset', err=True)
             sys.exit(1)
 
         if not yes:
             click.confirm(
-                f'Dataset "{name}" will be completely deleted.\nDo you want to continue?',
+                f'Dataset "{tbrn}" will be completely deleted.\nDo you want to continue?',
                 abort=True,
             )
 
         gas.delete_dataset(info.dataset_name)
-        click.echo(f'Dataset "{name}" is deleted successfully')
+        click.echo(f'Dataset "{tbrn}" is deleted successfully')
         return
 
-    if name:
-        if name.startswith("tb:"):
-            click.echo("Dataset name shouldn't start with 'tb:'")
+    if tbrn:
+        info = TBRN(tbrn=tbrn)
+        if info.type != TBRNType.DATASET:
+            click.echo(f'"{tbrn}" is not a dataset', err=True)
             sys.exit(1)
 
-        gas.create_dataset(name)
-        click.echo(f'Dataset "tb:{name}" is created successfully')
+        gas.create_dataset(info.dataset_name)
+        click.echo(f'Dataset "{tbrn}" is created successfully')
     else:
         for dataset_name in gas.list_dataset_names():
             click.echo(TBRN(dataset_name).get_tbrn())
