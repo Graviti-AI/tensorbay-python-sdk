64c155d9cf762005d5257751eafe8b01d3b4546f
rex.zheng <rex.zheng@graviti.com>
2021-05-27 11:23:10 +0800

    feat(client): adapt openAPI "createBranch"
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/613

--- tensorbay/client/dataset.py
+++ tensorbay/client/dataset.py
@@ -352,6 +352,23 @@ class DatasetClientBase:  # pylint: disable=too-many-public-methods
         """
         return PagingList(lambda offset, limit: self._generate_tags(None, offset, limit), 128)
 
+    def create_branch(self, name: str, revision: Optional[str] = None) -> None:
+        """Create the branch.
+
+        Arguments:
+            name: The branch name.
+            revision: The information to locate the specific commit, which can be the commit id,
+                the branch name, or the tag name.
+                If the revision is not given, create the branch based on the current commit.
+
+        """
+        if not revision:
+            self._status.check_authority_for_commit()
+            revision = self._status.commit_id
+
+        post_data: Dict[str, Any] = {"name": name, "commit": revision}
+        self._client.open_api_do("POST", "branches", self.dataset_id, json=post_data)
+
     def get_branch(self, name: str) -> Branch:
         """Get the branch with the given name.
 
