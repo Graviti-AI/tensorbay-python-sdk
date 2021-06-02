4babe16c75a07465bfcca74175fe037c9d93827f
rex.zheng <rex.zheng@graviti.com>
2021-06-02 11:11:01 +0800

    test(client): add integration test for creating and deleting commit
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/643

--- tests/test_branch.py
+++ tests/test_branch.py
@@ -6,12 +6,70 @@
 import pytest
 
 from tensorbay import GAS
-from tensorbay.exception import ResourceNotExistError
+from tensorbay.exception import ResourceNotExistError, ResponseSystemError
 
 from .utility import get_dataset_name
 
 
 class TestBranch:
+    def test_create_branch(self, accesskey, url):
+        gas_client = GAS(access_key=accesskey, url=url)
+        dataset_name = get_dataset_name()
+        dataset_client = gas_client.create_dataset(dataset_name)
+
+        # Creating the branch in a empty repo is not allowed
+        with pytest.raises(ResponseSystemError):
+            dataset_client.create_branch("T123")
+
+        dataset_client.create_draft("draft-1")
+        dataset_client.commit("commit-1", tag="V1")
+        dataset_client.create_branch("T123")
+        branch = dataset_client.get_branch("T123")
+
+        assert branch.name == "T123"
+        assert branch.commit_id == dataset_client.status.commit_id
+        assert branch.parent_commit_id is None
+        assert branch.message == "commit-1"
+        assert branch.committer.name
+        assert branch.committer.date
+
+        gas_client.delete_dataset(dataset_name)
+
+    def test_create_branch_on_revision(self, accesskey, url):
+        gas_client = GAS(access_key=accesskey, url=url)
+        dataset_name = get_dataset_name()
+        dataset_client = gas_client.create_dataset(dataset_name)
+
+        dataset_client.create_draft("draft-1")
+        dataset_client.commit("commit-1", tag="V1")
+        commit_id = dataset_client.status.commit_id
+        dataset_client.create_draft("draft-2")
+        dataset_client.commit("commit-2", tag="V2")
+
+        dataset_client.create_branch("Test_Commit_Id", revision=commit_id)
+        dataset_client.create_branch("Test_Tag", revision="V1")
+        dataset_client.create_branch("Test_Branch", revision="main")
+
+        branch = dataset_client.get_branch("Test_Commit_Id")
+        assert branch.name == "Test_Commit_Id"
+        assert branch.commit_id == commit_id
+        assert branch.parent_commit_id is None
+        assert branch.message == "commit-1"
+
+        branch = dataset_client.get_branch("Test_Tag")
+        assert branch.name == "Test_Tag"
+        assert branch.commit_id == commit_id
+        assert branch.parent_commit_id is None
+        assert branch.message == "commit-1"
+
+        branch = dataset_client.get_branch("Test_Branch")
+        assert branch.name == "Test_Branch"
+        assert branch.commit_id == dataset_client.status.commit_id
+        assert branch.parent_commit_id == commit_id
+        assert branch.message == "commit-2"
+
+        gas_client.delete_dataset(dataset_name)
+
     def test_get_branch(self, accesskey, url):
         gas_client = GAS(access_key=accesskey, url=url)
         dataset_name = get_dataset_name()
@@ -23,6 +81,8 @@ class TestBranch:
         dataset_client.commit("commit-2")
         commit_2_id = dataset_client.status.commit_id
 
+        dataset_client.create_branch("T123")
+
         branch = dataset_client.get_branch("main")
         assert branch.name == "main"
         assert branch.commit_id == commit_2_id
@@ -32,6 +92,14 @@ class TestBranch:
         assert branch.committer.date
         with pytest.raises(ResourceNotExistError):
             dataset_client.get_branch("main1")
+
+        branch1 = dataset_client.get_branch("T123")
+        assert branch1.name == "T123"
+        assert branch1.commit_id == commit_2_id
+        assert branch1.parent_commit_id == commit_1_id
+        assert branch1.message == "commit-2"
+        assert branch1.committer.name
+        assert branch1.committer.date
 
         gas_client.delete_dataset(dataset_name)
 
@@ -50,3 +118,16 @@ class TestBranch:
         assert branches[0].commit_id == commit_1_id
 
         gas_client.delete_dataset(dataset_name)
+
+    def test_delete_branch(self, accesskey, url):
+        gas_client = GAS(access_key=accesskey, url=url)
+        dataset_name = get_dataset_name()
+        dataset_client = gas_client.create_dataset(dataset_name)
+        dataset_client.create_draft("draft-1")
+        dataset_client.commit("commit-1")
+        dataset_client.create_branch("T123")
+
+        dataset_client.delete_branch("T123")
+
+        with pytest.raises(ResourceNotExistError):
+            dataset_client.get_branch("T123")
