7aea940c75476933610754957ea3f8b26ca22744
yexuan.li <yexuan.li@graviti.com>
2021-06-08 14:05:37 +0800

    feat(cli): support interactive authentication in "gas auth" command
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/681

--- tensorbay/cli/auth.py
+++ tensorbay/cli/auth.py
@@ -7,7 +7,10 @@
 
 
 from configparser import ConfigParser
-from typing import Dict
+from typing import Dict, Optional
+from urllib.parse import urljoin
+
+import click
 
 from .utility import error, form_profile_value, read_config, update_config, write_config
 
@@ -17,18 +20,18 @@ def _implement_auth(obj: Dict[str, str], arg1: str, arg2: str) -> None:
     config_parser = read_config()
 
     if not arg1 and not arg2:
-        error("Require accessKey to authenticate Tensorbay account")
+        arg1 = _interactive_auth()
 
-    if _is_accesskey(arg1):
-        if _is_url(arg2):
+    elif _is_accesskey(arg1):
+        if _is_gas_url(arg2):
             error('Please use "gas auth [url] [accessKey]" to specify the url and accessKey')
         if arg2:
             error(f'Redundant argument "{arg2}"')
 
-    elif _is_url(arg1):
+    elif _is_gas_url(arg1):
         if not arg2:
-            error("Require accessKey to authenticate Tensorbay account")
-        if not _is_accesskey(arg2):
+            arg2 = _interactive_auth(arg1)
+        elif not _is_accesskey(arg2):
             error("Wrong accesskey format")
     else:
         error(f'Invalid argument "{arg1}"')
@@ -37,12 +40,37 @@ def _implement_auth(obj: Dict[str, str], arg1: str, arg2: str) -> None:
     write_config(config_parser)
 
 
+def _interactive_auth(url: Optional[str] = None) -> str:
+    click.secho(
+        "Please visit and login to the TensorBay website to generate your AccessKey", bold=True
+    )
+    click.secho(
+        "Note: TensorBay has multi-regional websites, "
+        "please visit the corresponding website based on your location for better experience\n",
+        fg="bright_cyan",
+    )
+
+    if url:
+        developer_url = click.style(urljoin(url, "/tensorbay/developer"), underline=True)
+        click.echo(f" > {developer_url}\n")
+    else:
+        url_cn = click.style("https://gas.graviti.cn/tensorbay/developer", underline=True)
+        url_com = click.style("https://gas.graviti.com/tensorbay/developer", underline=True)
+        click.echo(f" > {url_com} (Global site)")
+        click.echo(f" > {url_cn} (Chinese site)\n")
+
+    access_key = click.prompt(click.style("Paste your AccessKey here", bold=True)).strip()
+    if not _is_accesskey(access_key):
+        error("Wrong accesskey format")
+    return access_key  # type: ignore[no-any-return]
+
+
 def _is_accesskey(arg: str) -> bool:
     return arg.startswith(("Accesskey-", "ACCESSKEY-")) and len(arg) == 42
 
 
-def _is_url(arg: str) -> bool:
-    return arg.startswith("https://")
+def _is_gas_url(arg: str) -> bool:
+    return arg.startswith("https://gas.")
 
 
 def _update_profile(config_parser: ConfigParser, profile_name: str, arg1: str, arg2: str) -> None:
