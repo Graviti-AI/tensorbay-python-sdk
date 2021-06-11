a95a5c354e5b24cfa7e8b5b0731060d182ddbc40
pengfei.wu <pengfei.wu@graviti.com>
2021-06-11 12:43:25 +0800

    feat(cli): support unsetting config option in "gas config" command
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/697

--- tensorbay/cli/config.py
+++ tensorbay/cli/config.py
@@ -12,7 +12,9 @@ import click
 from .utility import error, is_accesskey, read_config, update_config, write_config
 
 
-def _implement_config(key: str, value: str) -> None:
+def _implement_config(key: str, value: str, unset: bool) -> None:
+    _check_args_and_options(key, value, unset)
+
     update_config()
     config_parser = read_config()
 
@@ -36,13 +38,27 @@ def _implement_config(key: str, value: str) -> None:
             click.echo(f"{config_key} = {config_value}\n")
         return
 
-    if key != "editor":
-        error(f"The option '{key}' is not supported to configure currently.\n")
-
     if not value:
         if key not in config_section:
             error(f"{key} has not been configured yet")
+        if unset:
+            del config_section[key]
+            write_config(config_parser, show_message=False)
+            click.echo(f'Unset "{key}" successfully')
+            return
+
         click.echo(f"{key} = {config_section[key]}\n")
     else:
         config_section[key] = value
         write_config(config_parser)
+
+
+def _check_args_and_options(key: str, value: str, unset: bool) -> None:
+    if unset:
+        if value:
+            error('Use "--unset" option to unset config or use "key" and "value" to set config')
+        if not key:
+            error('Use "--unset" option with "key"')
+
+    if key and key != "editor":
+        error(f'The option "{key}" is not supported to configure currently.\n')
