17ab6e1965fa4ac8ddceb0099ad04b7452d736b6
yexuan.li <yexuan.li@graviti.com>
2021-06-18 13:52:18 +0800

    refactor(cli): add "--message" and deprecate "--title" in "gas draft"
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/735

--- tensorbay/cli/cli.py
+++ tensorbay/cli/cli.py
@@ -33,7 +33,8 @@ Use ``gas tag`` to list, create or delete tags.
 """
 
 from functools import partial
-from typing import Any, Dict, Iterable, Optional, Tuple
+from types import MethodType
+from typing import Any, Dict, Iterable, Optional, Sequence, Tuple, Union
 
 import click
 
@@ -49,7 +50,7 @@ class CustomCommand(click.Command):
     """
 
     def __init__(self, **kwargs: Any) -> None:
-        self.synopsis = kwargs.pop("synopsis", [])
+        self.synopsis = kwargs.pop("synopsis", ())
         super().__init__(**kwargs)
 
     def format_help(self, ctx: click.Context, formatter: click.HelpFormatter) -> None:
@@ -128,6 +129,153 @@ class DeprecatedCommand(CustomCommand):
         """
         click.secho(self.deprecated_message, fg="red", err=True)
         return super().invoke(ctx)
+
+
+class DeprecatedOption(click.Option):
+    """Customize deprecated option."""
+
+    def __init__(
+        self,
+        *args: Any,
+        deprecated: Union[str, Tuple[str, ...]],
+        since: str,
+        removed_in: Optional[str] = None,
+        preferred: Optional[Union[str, Tuple[str, ...]]] = None,
+        **kwargs: Any,
+    ) -> None:
+        if isinstance(deprecated, str):
+            self.deprecated: Tuple[str, ...] = (deprecated,)
+        else:
+            self.deprecated = deprecated
+        self.since = since
+        self.removed_in = removed_in
+        self.preferred = preferred
+        super().__init__(*args, **kwargs)
+
+    def join_options(self, options: Sequence[str]) -> Tuple[str, bool]:
+        """Join the options that are not deprecated.
+
+        Copied from ``click.formatting.join_options``
+        and add some code to exclude the deprecated opts in help message.
+
+        Arguments:
+            options: The options.
+
+        Returns:
+            The joined option string and whether there is slash prefix in options.
+
+        """
+        prefix_opt = []
+        any_prefix_is_slash = False
+
+        for opt in options:
+            if opt in self.deprecated:  # Exclude the deprecated opts in help message.
+                continue
+
+            prefix = click.parser.split_opt(opt)[0]
+
+            if prefix == "/":
+                any_prefix_is_slash = True
+
+            prefix_opt.append((len(prefix), opt))
+
+        prefix_opt.sort(key=lambda x: x[0])
+        return ", ".join(x[1] for x in prefix_opt), any_prefix_is_slash
+
+    def get_help_record(  # type: ignore[override]
+        self, ctx: click.Context
+    ) -> Optional[Tuple[str, str]]:
+        """Get help record.
+
+        Arguments:
+            ctx: The context of the option.
+
+        Returns:
+            The option help message.
+
+        """
+        if self.hidden:
+            return None
+
+        _, help_message = super().get_help_record(ctx)
+        any_prefix_is_slash = False
+
+        def _write_opts(opts: Sequence[str]) -> str:
+            nonlocal any_prefix_is_slash
+
+            # Use `self.join_options` instead of `click.formatting.join_options`
+            # to exclude the deprecated opts in help message.
+            opt_message, any_slashes = self.join_options(opts)
+
+            if any_slashes:
+                any_prefix_is_slash = True
+
+            if not self.is_flag and not self.count:
+                opt_message += f" {self.make_metavar()}"
+
+            return opt_message
+
+        opt_messages = [_write_opts(self.opts)]
+
+        if self.secondary_opts:
+            opt_messages.append(_write_opts(self.secondary_opts))
+
+        return ("; " if any_prefix_is_slash else " / ").join(opt_messages), help_message
+
+
+class DeprecatedOptionsCommand(CustomCommand):
+    """Customize command with deprecated options."""
+
+    def make_parser(self, ctx: click.Context) -> click.OptionParser:
+        """Hook 'make_parser' and check whether the name used to invoke the option is preferred.
+
+        Arguments:
+            ctx: The context of the command.
+
+        Returns:
+            The option parser.
+
+        """
+        parser = super().make_parser(ctx)
+
+        options = set(parser._short_opt.values()) | set(  # pylint: disable=protected-access
+            parser._long_opt.values()  # pylint: disable=protected-access
+        )
+
+        for option in options:
+            if not isinstance(option.obj, DeprecatedOption):
+                continue
+
+            def _process(
+                self: click.parser.Option,
+                value: Any,
+                state: click.parser.ParsingState,
+            ) -> None:
+                option_obj = self.obj
+
+                # reach up the stack and get 'opt'
+                import inspect
+
+                frame = inspect.currentframe()
+                if frame and frame.f_back:
+                    opt = frame.f_back.f_locals.get("opt")
+                else:
+                    opt = None
+
+                if opt in option_obj.deprecated:
+                    messages = [
+                        "DeprecationWarning: "
+                        f'The option "{opt}" is deprecated since version {option_obj.since}.'
+                    ]
+                    if option_obj.removed_in:
+                        messages.append(f"It will be removed in version {option_obj.removed_in}.")
+                    if option_obj.preferred:
+                        messages.append(f'Please use "{option_obj.preferred}" instead.')
+                    click.secho(" ".join(messages), fg="yellow")
+                return click.parser.Option.process(self, value, state)
+
+            option.process = MethodType(_process, option)  # type: ignore[assignment]
+        return parser
 
 
 @click.group()
@@ -249,20 +397,35 @@ def dataset(obj: Dict[str, str], tbrn: str, is_delete: bool, yes: bool) -> None:
 
 
 @command(
+    cls=DeprecatedOptionsCommand,
     synopsis=(
         "$ gas draft -l tb:<dataset_name>                                # List drafts.",
-        "$ gas draft tb:<dataset_name>[@<branch_name>] [-t <title>]      # Create a draft.",
-    )
+        "$ gas draft tb:<dataset_name>[@<branch_name>] [-m <message>]    # Create a draft.",
+    ),
 )
 @click.argument("tbrn", type=str)
 @click.option("-l", "--list", "is_list", is_flag=True, help="List the drafts.")
-@click.option("-t", "--title", type=str, default="", help="The title of the draft.")
+@click.option(
+    "-m",
+    "--message",
+    "-t",
+    "--title",
+    type=str,
+    multiple=True,
+    default=(),
+    help="The message of the draft.",
+    deprecated=("-t", "--title"),
+    preferred="-m",
+    since="v1.8.0",
+    removed_in="v1.10.0",
+    cls=DeprecatedOption,
+)
 @click.pass_obj
 def draft(
     obj: Dict[str, str],
     tbrn: str,
     is_list: bool,
-    title: str,
+    message: Tuple[str, ...],
 ) -> None:
     """List or create drafts.\f
 
@@ -270,12 +433,12 @@ def draft(
         obj: A dict contains config information.
         tbrn: The tbrn of the dataset.
         is_list: Whether to list the drafts.
-        title: The title of the draft.
+        message: The message of the draft.
 
     """  # noqa: D301,D415
     from .draft import _implement_draft
 
-    _implement_draft(obj, tbrn, is_list, title)
+    _implement_draft(obj, tbrn, is_list, message)
 
 
 @command(
