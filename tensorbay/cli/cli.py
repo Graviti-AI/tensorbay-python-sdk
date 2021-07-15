34f4cd9174469eee0bce6e038f49c10ad60ecb26
yexuan.li <yexuan.li@graviti.com>
2021-07-15 15:13:41 +0800

    docs(cli): correct the synopsis in gas commit help message
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/833

--- tensorbay/cli/cli.py
+++ tensorbay/cli/cli.py
@@ -211,7 +211,9 @@ def draft(  # pylint: disable=too-many-arguments
 
 
 @command(
-    synopsis=("$ gas commit tb:<dataset_name>#<draft_number> [-m <message>]      # Commit a draft.")
+    synopsis=(
+        "$ gas commit tb:<dataset_name>#<draft_number> [-m <message>]      # Commit a draft.",
+    )
 )
 @click.argument("tbrn", type=str)
 @click.option(
