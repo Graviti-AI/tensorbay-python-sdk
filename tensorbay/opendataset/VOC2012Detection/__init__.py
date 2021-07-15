11c3a8fc11707931418898bea4469cdfdbca5a44
yuanzhu.zhan <yuanzhu.zhan@graviti.com>
2021-07-15 14:04:54 +0800

    feat(opendataset): add dataloader for VOC2012Detection dataset
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/830

---
+++ tensorbay/opendataset/VOC2012Detection/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name
+
+"""Dataloader of VOC2012Detection."""
+
+from .loader import VOC2012Detection
+
+__all__ = ["VOC2012Detection"]
---
+++ tensorbay/opendataset/VOC2012Detection/loader.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name, missing-module-docstring
+
+import os
+import typing
+from xml.etree import ElementTree
+
+from ...dataset import Data, Dataset
+from ...label import LabeledBox2D
+
+_SEGMENT_NAMES = (
+    "train",
+    "trainval",
+    "val",
+)
+_BOOLEAN_ATTRIBUTES = {"occluded", "pose", "truncated"}
+DATASET_NAME = "VOC2012Detection"
+
+
+def VOC2012Detection(path: str) -> Dataset:
+    """Dataloader of the 'VOC2012Detection'_ dataset.
+
+    .. _VOC2012Detection: http://host.robots.ox.ac.uk/pascal/VOC/voc2012/
+
+    The file structure should be like::
+
+        <path>
+            Annotations/
+                <image_name>.xml
+                ...
+            JPEGImages/
+                <image_name>.jpg
+                ...
+            ImageSets/
+                Main/
+                    train.txt
+                    trainval.txt
+                    val.txt
+                    ...
+                ...
+            ...
+
+    Arguments:
+        path: The root directory of the dataset.
+
+    Returns:
+        Loaded :class: `~tensorbay.dataset.dataset.Dataset` instance.
+
+    """
+    paths = {
+        "Annotations": os.path.join(path, "Annotations"),
+        "Image": os.path.join(path, "JPEGImages"),
+        "Main": os.path.join(path, "ImageSets", "Main"),
+    }
+
+    dataset = Dataset(DATASET_NAME)
+    dataset.load_catalog("catalog.json")
+
+    for segment_name in _SEGMENT_NAMES:
+        segment = dataset.create_segment(segment_name)
+        with open(os.path.join(paths["Main"], f"{segment_name}.txt")) as fp:
+            for filename in fp:
+                filename = filename.strip()
+                segment.append(_get_data(filename, paths))
+    return dataset
+
+
+def _get_data(filename: str, paths: typing.Dict[str, str]) -> Data:
+    """Get all information of the datum corresponding to filename.
+
+    Arguments:
+        filename: The filename of the data.
+        paths: The dictionary includes paths.
+
+    Returns:
+        Data: class: `~tensorbay.dataset.data.Data` instance.
+
+    """
+    data = Data(os.path.join(paths["Image"], f"{filename}.jpg"))
+    data.label.box2d = []
+    tree = ElementTree.parse(os.path.join(paths["Annotations"], f"{filename}.xml"))
+    for obj in tree.findall("object"):
+        attributes = {}
+        for child in obj:
+            if child.tag == "name":
+                category = child.text
+            elif child.tag == "bndbox":
+                box = (
+                    int(child.find("xmin").text),  # type:ignore[arg-type, union-attr]
+                    int(child.find("ymin").text),  # type:ignore[arg-type, union-attr]
+                    int(child.find("xmax").text),  # type:ignore[arg-type, union-attr]
+                    int(child.find("ymax").text),  # type:ignore[arg-type, union-attr]
+                )
+            elif child.tag == "pose":
+                attributes[child.tag] = child.text
+            elif child.tag in _BOOLEAN_ATTRIBUTES:
+                attributes[child.tag] = bool(
+                    int(child.text)  # type:ignore[assignment, arg-type]
+                )
+        data.label.box2d.append(LabeledBox2D(*box, category=category, attributes=attributes))
+    return data
