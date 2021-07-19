6fc367c3ab2d55eec1904b034d44327fd789c1b2
shunwei.yuan <shunwei.yuan@graviti.com>
2021-07-19 17:59:59 +0800

    feat(opendataset): add polygon & polyline2d to "BDD100K"
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/846

--- tensorbay/opendataset/BDD100K/loader.py
+++ tensorbay/opendataset/BDD100K/loader.py
@@ -8,16 +8,29 @@
 
 import json
 import os
+from glob import glob
 from typing import Any, Dict, List
+from warnings import warn
 
 from ...dataset import Data, Dataset
-from ...label import Classification, LabeledBox2D
+from ...label import Classification, LabeledBox2D, LabeledPolygon, LabeledPolyline2D
 from ...opendataset import _utility
 
-DATASET_NAME = "BDD100K"
-_IMAGE_FOLDER = "Images/100k"
-_LABELS_FOLDER = "Labels"
-_MODES = ("test", "train", "val")
+DATASET_NAMES = {"100k": "BDD100K", "10k": "BDD100K_10K"}
+
+_SEGMENT_NAMES = ("test", "train", "val")
+_LABEL_TYPE_INFO = {
+    "100k": {
+        "det": ("Detection 2020", "BOX2D"),
+        "lane": ("Lane Marking", "POLYLINE2D"),
+        "drivable": ("Drivable Area", "POLYGON"),
+    },
+    "10k": {
+        "ins_seg": ("Instance Segmentation", "POLYGON"),
+        "sem_seg": ("Semantic Segmentation", "POLYLINE2D"),
+        "pan_seg": ("Panoptic Segmentation", "POLYLINE2D"),
+    },
+}
 
 
 def BDD100K(path: str) -> Dataset:
@@ -26,18 +39,24 @@ def BDD100K(path: str) -> Dataset:
     The file structure should be like::
 
         <path>
-            Images/
-                100k/
-                    test
-                    train
-                    val
-                10k/
-                    test
-                    train
-                    val
-            Labels/
-                bdd100k_labels_images_train.json
-                bdd100k_labels_images_val.json
+            bdd100k_images_100k/
+                images/
+                    100k/
+                        test
+                        train
+                        val
+                labels/
+                    det_20/
+                        det_train.json
+                        det_val.json
+                    lane/
+                        polygons/
+                            lane_train.json
+                            lane_val.json
+                    drivable/
+                        polygons/
+                            drivable_train.json
+                            drivable_val.json
 
     Arguments:
         path: The root directory of the dataset.
@@ -46,86 +65,187 @@ def BDD100K(path: str) -> Dataset:
         Loaded :class:`~tensorbay.dataset.dataset.Dataset` instance.
 
     """
-    root_path = os.path.abspath(os.path.expanduser(path))
-    dataset = Dataset(DATASET_NAME)
-    dataset.load_catalog(os.path.join(os.path.dirname(__file__), "catalog.json"))
+    return _BDD100K_loader(path, "100k")
 
-    label_path = {
-        "train": os.path.join(root_path, _LABELS_FOLDER, "bdd100k_labels_images_train.json"),
-        "val": os.path.join(root_path, _LABELS_FOLDER, "bdd100k_labels_images_val.json"),
-        "test": "",
-    }
 
-    for mode in _MODES:
-        _load_segment(
-            dataset=dataset,
-            path=root_path,
-            mode=mode,
-            label_file_path=label_path[mode],
-        )
+def _BDD100K_10K(path: str) -> Dataset:
+    """Load a sub-dataset 10k of BDD100K Dataset to Tensorbay.
+
+    The dataset is named as 'BDD100K_10K'
+
+    The file structure should be like::
+
+        <path>
+            bdd100k_images_10k/
+                images/
+                    10k/
+                        test
+                        train
+                        val
+                labels/
+                    ins_seg/
+                        polygons/
+                            ins_seg_train.json
+                            ins_seg_val.json
+                    pan_seg/
+                        polygons/
+                            pan_seg_train.json
+                            pan_seg_val.json
+                    sem_seg/
+                        polygons/
+                            sem_seg_train.json
+                            sem_seg_val.json
+
+    Arguments:
+        path: The root directory of the dataset.
+
+    Returns:
+        Loaded :class:`~tensorbay.dataset.dataset.Dataset` instance.
+
+    """
+    return _BDD100K_loader(path, "10k")
+
+
+def _BDD100K_loader(path: str, dataset_type: str) -> Dataset:
+    root_path = os.path.join(
+        os.path.abspath(os.path.expanduser(path)), f"bdd100k_images_{dataset_type}"
+    )
+    dataset = Dataset(DATASET_NAMES[dataset_type])
+    dataset.load_catalog(os.path.join(os.path.dirname(__file__), f"catalog_{dataset_type}.json"))
+
+    _load_segment(dataset, root_path, dataset_type)
 
     return dataset
 
 
-def _load_segment(
-    dataset: Dataset,
-    path: str,
-    mode: str,
-    label_file_path: str,
+def _load_segment(dataset: Dataset, root_path: str, dataset_type: str) -> None:
+    images_directory = os.path.join(root_path, "images", dataset_type)
+    labels_directory = os.path.join(root_path, "labels")
+
+    get_data = _get_data_100k if dataset_type == "100k" else _get_data_10k
+
+    for segment_name in _SEGMENT_NAMES:
+        segment = dataset.create_segment(segment_name)
+        image_paths = _utility.glob(os.path.join(images_directory, segment_name, "*.jpg"))
+
+        print(f"Reading data to segment '{segment_name}'...")
+        if segment_name == "test":
+            for image_path in image_paths:
+                segment.append(Data(image_path))
+        else:
+            label_contents = _read_label_file(labels_directory, segment_name, dataset_type)
+            for image_path in image_paths:
+                segment.append(get_data(image_path, label_contents[os.path.basename(image_path)]))
+        print(f"Finished reading data to segment '{segment_name}'")
+
+
+def _get_data_10k(image_path: str, label_content: Dict[str, Any]) -> Data:
+    data = Data(image_path)
+    polygon: List[LabeledPolygon] = []
+    polyline2d: List[LabeledPolyline2D] = []
+    for label_info in label_content["labels"]:
+        if "poly2d" in label_info:
+            _add_poly2d_label(label_info, polygon, polyline2d)
+    data.label.polygon = polygon
+    data.label.polyline2d = polyline2d
+    return data
+
+
+def _get_data_100k(image_path: str, label_content: Dict[str, Any]) -> Data:
+    data = Data(image_path)
+    box2d: List[LabeledBox2D] = []
+    polygon: List[LabeledPolygon] = []
+    polyline2d: List[LabeledPolyline2D] = []
+    data.label.classification = Classification(attributes=label_content["attributes"])
+    for label_info in label_content["labels"]:
+        if "box2d" in label_info:
+            _add_box2d_label(label_info, box2d)
+        if "poly2d" in label_info:
+            _add_poly2d_label(label_info, polygon, polyline2d)
+    data.label.box2d = box2d
+    data.label.polygon = polygon
+    data.label.polyline2d = polyline2d
+    return data
+
+
+def _add_box2d_label(label_info: Dict[str, Any], box2d: List[LabeledBox2D]) -> None:
+    box2d_info = label_info["box2d"]
+    labeled_box2d = LabeledBox2D(
+        box2d_info["x1"],
+        box2d_info["y1"],
+        box2d_info["x2"],
+        box2d_info["y2"],
+        category=label_info["category"],
+        attributes=label_info["attributes"],
+    )
+    box2d.append(labeled_box2d)
+
+
+def _add_poly2d_label(
+    label_info: Dict[str, Any], polygon: List[LabeledPolygon], polyline2d: List[LabeledPolyline2D]
 ) -> None:
-    segment = dataset.create_segment(mode)
-    segment_path = os.path.join(path, _IMAGE_FOLDER, mode)
-    image_paths = _utility.glob(os.path.join(path, _IMAGE_FOLDER, mode, "*.jpg"))
-    unlabeled_image_names = []
-    if mode == "test":
-        for image_path in image_paths:
-            segment.append(Data(image_path))
-        return
+    poly2d_info = label_info["poly2d"][0]
+    if poly2d_info["closed"]:
+        labeled_polygon = LabeledPolygon(
+            points=poly2d_info["vertices"],
+            category=label_info["category"],
+            attributes=label_info["attributes"],
+        )
+        polygon.append(labeled_polygon)
+    else:
+        labeled_polyline2d = LabeledPolyline2D(
+            points=poly2d_info["vertices"],
+            category=label_info["category"],
+            attributes=label_info.get("attributes", {}),
+        )
+        polyline2d.append(labeled_polyline2d)
 
-    with open(label_file_path) as fp:
-        labels = json.load(fp)
 
-    image_name_to_label = {}
-    for label in labels:
-        image_name_to_label[label["name"]] = label
+def _read_label_file(label_directory: str, segment_name: str, dataset_type: str) -> Dict[str, Any]:
+    source_label_contents = []
+    label_filenames = glob(
+        os.path.join(label_directory, "**", f"*_{segment_name}.json"), recursive=True
+    )
 
-    for image_path in image_paths:
-        image_name = os.path.basename(image_path)
-        if image_name not in image_name_to_label:
-            unlabeled_image_names.append(image_name)
+    label_type_info = _LABEL_TYPE_INFO[dataset_type]
+    label_prefixes = set(label_type_info)
+    for label_filename in label_filenames:
+        label_file_basename = os.path.basename(label_filename)
+        label_prefix = label_file_basename.replace(f"_{segment_name}.json", "")
+        try:
+            label_prefixes.remove(label_prefix)
+        except KeyError:
+            warn_message = f"Invalid label file name '{label_file_basename}'! Ignoring.."
+            warn(warn_message)
             continue
 
-        data = Data(image_path)
-        _add_label(data, image_name_to_label[image_name])
-        segment.append(data)
+        label_description = label_type_info[label_prefix][0]
+        print(f"Reading '{label_description}' labels to segment '{segment_name}'...")
+        with open(label_filename, "r") as fp:
+            source_label_contents.append(json.load(fp))
+        print(f"Finished reading '{label_description}' labels to segment '{segment_name}'...")
 
-    if unlabeled_image_names:
-        _load_unlabeled_segment(dataset, unlabeled_image_names, mode, segment_path)
+    for missing_label_prefix in label_prefixes:
+        warn_message = (
+            f"Missing label file '{missing_label_prefix}_{segment_name}.json'! "
+            f"The correspondent '{label_type_info[missing_label_prefix][1]}' "
+            f"label will be set to empty!"
+        )
+        warn(warn_message)
+
+    print(f"Merging '{segment_name}' labels...")
+    label_contents = _merge_label(source_label_contents)
+    print(f"Finished merging '{segment_name}' labels")
+    return label_contents
 
 
-def _load_unlabeled_segment(
-    dataset: Dataset, unlabeled_image_names: List[str], mode: str, segment_path: str
-) -> None:
-    segment = dataset.create_segment(f"unlabeled_images_in_{mode}")
-    for unlabeled_image_name in unlabeled_image_names:
-        data = Data(os.path.join(segment_path, unlabeled_image_name))
-        segment.append(data)
+def _merge_label(source_label_contents: List[List[Dict[str, Any]]]) -> Dict[str, Any]:
+    label_contents: Dict[str, Any] = {}
+    for source_label_content in source_label_contents:
+        for image_info in source_label_content:
+            image_name = image_info["name"]
+            image_label = label_contents.setdefault(image_name, {})
+            image_label.setdefault("labels", []).extend(image_info.get("labels", []))
+            image_label.setdefault("attributes", {}).update(image_info.get("attributes", {}))
 
-
-def _add_label(data: Data, labels: Dict[str, Any]) -> None:
-    data.label.classification = Classification(attributes=labels["attributes"])
-    box2d_labels = []
-    for label in labels["labels"]:
-        if "box2d" in label:
-            box2d_info = label["box2d"]
-            box2d_label = LabeledBox2D(
-                box2d_info["x1"],
-                box2d_info["y1"],
-                box2d_info["x2"],
-                box2d_info["y2"],
-                category=label["category"],
-                attributes=label["attributes"],
-            )
-            box2d_labels.append(box2d_label)
-
-    data.label.box2d = box2d_labels
+    return label_contents
