5d34e5057aedaed1aea1d84f5b82a9adc40dae49
zhen.chen <zhen.chen@graviti.com>
2021-08-05 11:57:22 +0800

    test: add case about importing cloud files from azure into tensorbay
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/891

---
+++ tests/test_cloud.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+
+import pytest
+
+from tensorbay import GAS
+from tensorbay.dataset import Dataset, Segment
+from tensorbay.exception import ResourceNotExistError
+
+from .utility import get_dataset_name
+
+
+class TestImportData:
+    def test_import_dataset_from_azure(self, accesskey, url):
+
+        gas_client = GAS(access_key=accesskey, url=url)
+        try:
+            cloud_client = gas_client.get_cloud_client("azure_china_config")
+        except ResourceNotExistError:
+            pytest.skip("skip this case because there's no 'azure_china_config' config")
+
+        source_prefix, dataset_prefix = "test/02", "test/03"
+        auth_data = cloud_client.list_auth_data(source_prefix)
+        dataset_name = get_dataset_name()
+        dataset_client = gas_client.create_auth_dataset(
+            dataset_name, "azure_china_config", dataset_prefix
+        )
+
+        dataset = Dataset(name=dataset_name)
+        segment = dataset.create_segment("Segment1")
+        for data in auth_data:
+            segment.append(data)
+
+        dataset_client = gas_client.upload_dataset(dataset, jobs=5)
+        dataset_client.commit("import data")
+
+        segment1 = Segment("Segment1", client=dataset_client)
+        assert len(segment1) == len(segment)
+        assert segment1[0].path == segment[0].path.split("/")[-1]
+        assert not segment1[0].label
+
+        assert len(cloud_client.list_auth_data(dataset_prefix)) == len(segment)
+
+        gas_client.delete_dataset(dataset_name)
