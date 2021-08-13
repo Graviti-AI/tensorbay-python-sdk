2be7ffb9de2edb8859cec1080affb835ac8bb9a3
rex.zheng <rex.zheng@graviti.com>
2021-08-13 17:06:17 +0800

    test(client): simplify and standardize the integration test about branch
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/930

--- tests/test_branch.py
+++ tests/test_branch.py
@@ -8,7 +8,7 @@ import pytest
 from tensorbay import GAS
 from tensorbay.client.gas import DEFAULT_BRANCH
 from tensorbay.client.struct import ROOT_COMMIT_ID
-from tensorbay.exception import InvalidParamsError, OperationError, ResourceNotExistError
+from tensorbay.exception import ForbiddenError, OperationError, ResourceNotExistError
 
 from .utility import get_dataset_name
 
@@ -20,23 +20,16 @@ class TestBranch:
         dataset_client = gas_client.create_dataset(dataset_name)
 
         # Creating the branch in a empty dataset is not allowed
-        with pytest.raises(InvalidParamsError):
+        with pytest.raises(ForbiddenError):
             dataset_client.create_branch("T123")
 
         dataset_client.create_draft("draft-1")
         dataset_client.commit("commit-1", "test", tag="V1")
-        commit_id = dataset_client.status.commit_id
+        commit_1_id = dataset_client.status.commit_id
         dataset_client.create_branch("T123")
 
-        assert dataset_client._dataset_id is not None
-        assert dataset_client._status.commit_id == commit_id
-        assert dataset_client._status.draft_number is None
-        assert dataset_client._status.branch_name == "T123"
-
         branch = dataset_client.get_branch("T123")
-
-        assert branch.name == "T123"
-        assert branch.commit_id == dataset_client.status.commit_id
+        assert branch.commit_id == commit_1_id
         assert branch.parent_commit_id == ROOT_COMMIT_ID
         assert branch.title == "commit-1"
         assert branch.description == "test"
@@ -59,19 +52,13 @@ class TestBranch:
 
         # Create a branch based on commit id
         dataset_client.create_branch("Test_Commit_Id", revision=commit_id_1)
-        assert dataset_client._dataset_id is not None
-        assert dataset_client._status.commit_id == commit_id_1
-        assert dataset_client._status.draft_number is None
-        assert dataset_client._status.branch_name == "Test_Commit_Id"
-
-        # Create a branch based on tag
         branch = dataset_client.get_branch("Test_Commit_Id")
         assert branch.name == "Test_Commit_Id"
         assert branch.commit_id == commit_id_1
         assert branch.parent_commit_id == ROOT_COMMIT_ID
         assert branch.title == "commit-1"
 
-        # Create a branch based on branch
+        # Create a branch based on tag
         dataset_client.create_branch("Test_Tag", revision="V1")
         branch = dataset_client.get_branch("Test_Tag")
         assert branch.name == "Test_Tag"
@@ -79,6 +66,7 @@ class TestBranch:
         assert branch.parent_commit_id == ROOT_COMMIT_ID
         assert branch.title == "commit-1"
 
+        # Create a branch based on branch
         dataset_client.create_branch("Test_Branch", revision=DEFAULT_BRANCH)
         branch = dataset_client.get_branch("Test_Branch")
         assert branch.name == "Test_Branch"
@@ -93,33 +81,33 @@ class TestBranch:
         dataset_name = get_dataset_name()
         dataset_client = gas_client.create_dataset(dataset_name)
         dataset_client.create_draft("draft-1")
-        dataset_client.commit("commit-1", "test", tag="V1")
+        dataset_client.commit("commit-1")
         commit_1_id = dataset_client.status.commit_id
         dataset_client.create_draft("draft-2")
-        dataset_client.commit("commit-2")
+        dataset_client.commit("commit-2", "test")
         commit_2_id = dataset_client.status.commit_id
-
-        dataset_client.create_branch("T123")
 
         branch = dataset_client.get_branch(DEFAULT_BRANCH)
         assert branch.name == DEFAULT_BRANCH
         assert branch.commit_id == commit_2_id
         assert branch.parent_commit_id == commit_1_id
         assert branch.title == "commit-2"
-        assert branch.description == ""
+        assert branch.description == "test"
         assert branch.committer.name
         assert branch.committer.date
         with pytest.raises(ResourceNotExistError):
             dataset_client.get_branch("main1")
 
-        branch1 = dataset_client.get_branch("T123")
-        assert branch1.name == "T123"
-        assert branch1.commit_id == commit_2_id
-        assert branch1.parent_commit_id == commit_1_id
-        assert branch1.title == "commit-2"
-        assert branch.description == ""
-        assert branch1.committer.name
-        assert branch1.committer.date
+        dataset_client.create_branch("T123")
+
+        branch = dataset_client.get_branch("T123")
+        assert branch.name == "T123"
+        assert branch.commit_id == commit_2_id
+        assert branch.parent_commit_id == commit_1_id
+        assert branch.title == "commit-2"
+        assert branch.description == "test"
+        assert branch.committer.name
+        assert branch.committer.date
 
         gas_client.delete_dataset(dataset_name)
 
@@ -128,8 +116,7 @@ class TestBranch:
         dataset_name = get_dataset_name()
         dataset_client = gas_client.create_dataset(dataset_name)
         dataset_client.create_draft("draft-1")
-        dataset_client.commit("commit-1", tag="V1")
-
+        dataset_client.commit("commit-1")
         commit_1_id = dataset_client.status.commit_id
 
         branches = dataset_client.list_branches()
@@ -146,6 +133,9 @@ class TestBranch:
         dataset_client.create_draft("draft-1")
         dataset_client.commit("commit-1")
         dataset_client.create_branch("T123")
+        dataset_client.create_draft("draft-2")
+        dataset_client.commit("commit-2")
+        commit_2_id = dataset_client.status.commit_id
 
         # Deleting the current branch is not allowed
         with pytest.raises(OperationError):
@@ -156,3 +146,6 @@ class TestBranch:
 
         with pytest.raises(ResourceNotExistError):
             dataset_client.get_branch("T123")
+
+        # After deleting the branch, the commit in the branch will not be deleted
+        dataset_client.get_commit(commit_2_id)
