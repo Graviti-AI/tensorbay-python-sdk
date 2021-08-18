0ee6bce2312b88624b71c434953fd86f83c1a449
rex.zheng <rex.zheng@graviti.com>
2021-08-18 17:21:49 +0800

    test: simplify and standardize the integration test about branch
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/945

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -6,12 +6,12 @@
 import pytest
 
 from tensorbay import GAS
-from tensorbay.exception import StatusError
+from tensorbay.exception import InvalidParamsError
 from tensorbay.label import Catalog
 
 from .utility import get_dataset_name
 
-CATALOG = {
+CATALOG1 = {
     "BOX2D": {
         "categories": [
             {"name": "01"},
@@ -43,17 +43,76 @@ CATALOG = {
 }
 
 
+CATALOG2 = {
+    "BOX2D": {
+        "categories": [
+            {"name": "01"},
+            {"name": "02"},
+            {"name": "03"},
+            {"name": "04"},
+            {"name": "05"},
+        ],
+        "attributes": [
+            {"name": "Vertical angle", "enum": [-90, -60, -30, -15, 0, 15, 30, 60, 90]},
+            {
+                "name": "Horizontal angle",
+                "enum": [-90, -75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75, 90],
+            },
+            {"name": "Serie", "enum": [1, 2]},
+        ],
+    }
+}
+
+
 class TestCatalog:
-    def test_catalog(self, accesskey, url):
+    def test_first_upload_catalog(self, accesskey, url):
         gas_client = GAS(access_key=accesskey, url=url)
         dataset_name = get_dataset_name()
         dataset_client = gas_client.create_dataset(dataset_name)
-
-        with pytest.raises(StatusError):
-            dataset_client.upload_catalog(Catalog.loads(CATALOG))
+        catalog_to_upload = Catalog.loads(CATALOG1)
         dataset_client.create_draft("draft-1")
-        dataset_client.upload_catalog(Catalog.loads(CATALOG))
-        catalog = dataset_client.get_catalog()
-        # todo: match the input and output catalog
+
+        dataset_client.upload_catalog(catalog_to_upload)
+        catalog_get = dataset_client.get_catalog()
+        assert catalog_get == catalog_to_upload
+
+        gas_client.delete_dataset(dataset_name)
+
+    def test_modify_catalog(self, accesskey, url):
+        gas_client = GAS(access_key=accesskey, url=url)
+        dataset_name = get_dataset_name()
+        dataset_client = gas_client.create_dataset(dataset_name)
+        dataset_client.create_draft("draft-1")
+        dataset_client.upload_catalog(Catalog.loads(CATALOG2))
+
+        catalog_to_upload = Catalog.loads(CATALOG2)
+        dataset_client.upload_catalog(catalog_to_upload)
+        catalog_get = dataset_client.get_catalog()
+        assert catalog_get == catalog_to_upload
+
+        # Uploading empty catalog is not allowed
+        catalog_to_upload = Catalog.loads({})
+        with pytest.raises(InvalidParamsError):
+            dataset_client.upload_catalog(catalog_to_upload)
+
+        gas_client.delete_dataset(dataset_name)
+
+    def test_catalog_version_control(self, accesskey, url):
+        gas_client = GAS(access_key=accesskey, url=url)
+        dataset_name = get_dataset_name()
+        dataset_client = gas_client.create_dataset(dataset_name)
+        catalog_to_upload = Catalog.loads(CATALOG1)
+        dataset_client.create_draft("draft-1")
+        dataset_client.upload_catalog(catalog_to_upload)
+
+        # After committing the draft, catalog is stored in the new commit
+        dataset_client.commit("commit-1")
+        catalog_get_commit = dataset_client.get_catalog()
+        assert catalog_get_commit == catalog_to_upload
+
+        # After creating a new draft, catalog is stored in the new draft
+        dataset_client.create_draft("draft-2")
+        catalog_get_draft = dataset_client.get_catalog()
+        assert catalog_get_draft == catalog_to_upload
 
         gas_client.delete_dataset(dataset_name)
