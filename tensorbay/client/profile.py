f6d8ffc7fcb6fee7abb2ac3f31d58ecf7dd5e85e
zhen.chen <zhen.chen@graviti.com>
2021-09-09 16:33:11 +0800

    feat(client): support save Profile summary to csv, txt or json file
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/1004

--- tensorbay/client/profile.py
+++ tensorbay/client/profile.py
@@ -9,19 +9,30 @@
 
 """
 
+import csv
 import json
 import time
-from collections import defaultdict
+from collections import OrderedDict, defaultdict
 from functools import wraps
+from itertools import chain
 from multiprocessing import Manager
 from multiprocessing.managers import SyncManager
-from typing import Any, Callable, DefaultDict, Dict, TypeVar
+from typing import Any, Callable, DefaultDict, Dict, Optional, TypeVar
+from urllib.parse import urlparse
 
 from requests.models import Response
 
 from .requests import Client
 
 _Callable = TypeVar("_Callable", bound=Callable[..., Response])
+_Columns = OrderedDict(
+    totalTime=12,
+    callNumber=11,
+    avgTime=10,
+    totalResponseLength=20,
+    totalFileSize=16,
+)
+_PATH_PREFIX = "/gatewayv2/tensorbay-open-api/v1/"
 
 
 class Profile:
@@ -45,6 +56,34 @@ class Profile:
             value["avgTime"] = value["totalTime"] / value["callNumber"]
             self._summary[key] = value
 
+    @staticmethod
+    def _format_string(path: str = "Path", item: Optional[Dict[str, float]] = None) -> str:
+        content = [f"| {path:<58}"]
+        if item is None:
+            content.extend(f"{key:<{value}}" for key, value in _Columns.items())
+        else:
+            content.extend(f"{item[key]:<{value}.3f}" for key, value in _Columns.items())
+        content.append("\n")
+        return " |".join(content)
+
+    def _save_to_txt(self, path: str) -> None:
+        with open(path, "w") as fp:
+            fp.write(self._format_string())
+            for url, item in self._summary.items():
+                fp.write(self._format_string(url, item))
+
+    def _save_to_csv(self, path: str) -> None:
+        with open(path, "w") as fp:
+            writer = csv.writer(fp)
+            writer.writerow(chain(["Path"], _Columns.keys()))
+            for post, item in self._summary.items():
+                writer.writerow(chain([post], (item[key] for key in _Columns)))
+
+    def _save_to_json(self, path: str) -> None:
+        summary = self._summary if isinstance(self._summary, dict) else self._summary.copy()
+        with open(path, "w") as fp:
+            json.dump(summary, fp, indent=4)
+
     def statistical(self, func: _Callable) -> _Callable:
         """A decorator to record the running time, response length ..etc of the function.
 
@@ -58,7 +97,7 @@ class Profile:
 
         @wraps(func)
         def wrapper(client: Client, method: str, url: str, **kwargs: Any) -> Response:
-            key = f"[{method}] {url}"
+            key = f"[{method}] {urlparse(url).path.replace(_PATH_PREFIX, '')}"
             data_length = getattr(kwargs.get("data"), "len", 0)
             start_time = time.time()
             response = func(client, method, url, **kwargs)
@@ -86,17 +125,17 @@ class Profile:
         item["totalFileSize"] += file_size
         self._summary[key] = item
 
-    def save(self, path: str) -> None:
+    def save(self, path: str, file_type: str = "txt") -> None:
         """Save the statistical summary into a file.
 
         Arguments:
             path: The file local path.
+            file_type: Type of the save file, only support txt, json, csv.
 
         """
         self._calculate_average_time()
-        summary = self._summary if isinstance(self._summary, dict) else self._summary.copy()
-        with open(path, "w") as fp:
-            json.dump(summary, fp, indent=4)
+        writers = {"txt": self._save_to_txt, "json": self._save_to_json, "csv": self._save_to_csv}
+        writers[file_type](path)
 
     def start(self, multiprocess: bool = False) -> None:
         """Start statistical record.
@@ -110,7 +149,6 @@ class Profile:
             self._summary = self._manager.dict()
         else:
             self._summary = {}
-
         Client.do = self.statistical(self.do_function)  # type: ignore[assignment]
 
     def stop(self) -> None:
