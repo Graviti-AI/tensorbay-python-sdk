c257166cf6d4f29089f29b7d426c87ec458bdda2
xin.wang <xin.wang@graviti.com>
2021-10-21 11:03:24 +0800

    feat(opendataset): add dataloader for SCUT_FBP5500
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/1076

---
+++ tensorbay/opendataset/SCUT_FBP5500/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# Copytright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name
+
+"""Dataloader of the SCUT_FBP5500 dataset."""
+
+from tensorbay.opendataset.SCUT_FBP5500.loader import SCUT_FBP5500
+
+__all__ = ["SCUT_FBP5500"]
