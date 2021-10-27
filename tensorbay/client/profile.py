475922ab6ad6652219016d2890e3e5b1fb1b6830
zhen.chen <zhen.chen@graviti.com>
2021-10-27 11:26:31 +0800

    feat(client): support record statistical of "Data.open().read()"
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/1096

--- tensorbay/client/profile.py
+++ tensorbay/client/profile.py
@@ -11,11 +11,15 @@
 
 import csv
 import json
+import math
+import time
 from collections import OrderedDict, defaultdict
 from functools import wraps
+from http.client import HTTPResponse
 from itertools import chain
 from multiprocessing import Manager
 from multiprocessing.managers import SyncManager
+from types import MethodType
 from typing import Any, Callable, DefaultDict, Dict, Optional, TypeVar
 from urllib.parse import urlparse
 
@@ -23,19 +27,42 @@ from requests.models import Response
 from requests_toolbelt.multipart.encoder import FileWrapper, MultipartEncoder
 
 from tensorbay.client.requests import Client
+from tensorbay.utility.file import RemoteFileMixin
 
 _Callable = TypeVar("_Callable", bound=Callable[..., Response])
+_OpenCallable = TypeVar("_OpenCallable", bound=Callable[..., HTTPResponse])
+_ReadCallable = Callable[..., bytes]
 
 _COLUMNS = OrderedDict(
     totalTime="{:<20.3f}",
     callNumber="{:<20}",
     avgTime="{:<20.3f}",
     totalResponseLength="{:<20}",
-    totalFileSize="{:<20}",
+    totalFileSize="{:<13}",
+    speed="{:<13}",
 )
 _TITLE = f"{'totalTime (s)':<20}{' |callNumber':<18}\
-    {' |avgTime (s)':<22}{' |totalResponseLength':<22}{' |totalFileSize (B)':<22}"
+    {' |avgTime (s)':<22}{' |totalResponseLength':<22}{' |totalFileSize':<15}{' |speed':<15}"
 _PATH_PREFIX = "/gatewayv2/tensorbay-open-api/v1/"
+_UNITS = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+
+
+def format_size(size: float) -> str:
+    """Format a byte count as a human readable file size.
+
+    Arguments:
+        size: The size to format in bytes.
+
+    Returns:
+        The human readable string.
+
+    """
+    if size == 0:
+        return "0B"
+
+    index = int(math.log(size, 1024))
+    number = pow(1024, index)
+    return f"{size/number:.2f}{_UNITS[index]}"
 
 
 class Profile:
@@ -46,6 +73,7 @@ class Profile:
 
     def __init__(self) -> None:
         self.do_function = Client.do
+        self.urlopen = RemoteFileMixin._urlopen
 
     def __enter__(self) -> "Profile":
         self.start()
@@ -54,9 +82,11 @@ class Profile:
     def __exit__(self, *_: Any) -> None:
         self.stop()
 
-    def _calculate_average_time(self) -> None:
+    def _calculate_and_format(self) -> None:
         for key, value in self._summary.items():
             value["avgTime"] = value["totalTime"] / value["callNumber"]
+            value["totalFileSize"] = format_size(value["rawTotalFileSize"])
+            value["speed"] = f'{format_size(value["rawTotalFileSize"] / value["totalTime"])}/s'
             self._summary[key] = value
 
     @staticmethod
@@ -121,6 +151,33 @@ class Profile:
 
         return wrapper  # type: ignore[return-value]
 
+    def _statistical_read(self, download_path: str) -> _ReadCallable:
+        def wrapper(response: HTTPResponse, amt: Optional[int] = None) -> bytes:
+            start_time = time.time()
+            content = HTTPResponse.read(response, amt)
+            self._update(download_path, 0, time.time() - start_time, len(content))
+            return content
+
+        return wrapper
+
+    def _statistical_open(self, func: _OpenCallable) -> _OpenCallable:
+        @wraps(func)
+        def wrapper(obj: RemoteFileMixin) -> HTTPResponse:
+            netloc = urlparse(obj.url.get()).netloc  # type: ignore[union-attr]
+            download_path = f"[GET] {netloc}/*"
+
+            start_time = time.time()
+            fp = func(obj)
+            cost_time = time.time() - start_time
+
+            item = self._summary.get(download_path, defaultdict(int))
+            item["totalTime"] += cost_time
+            self._summary[download_path] = item
+            setattr(fp, "read", MethodType(self._statistical_read(download_path), fp))
+            return fp
+
+        return wrapper  # type: ignore[return-value]
+
     def _update(self, key: str, response_length: int, cost_time: float, file_size: int) -> None:
         """Update the giving information into summary.
 
@@ -135,7 +192,7 @@ class Profile:
         item["totalTime"] += cost_time
         item["callNumber"] += 1
         item["totalResponseLength"] += response_length
-        item["totalFileSize"] += file_size
+        item["rawTotalFileSize"] += file_size
         self._summary[key] = item
 
     def save(self, path: str, file_type: str = "txt") -> None:
@@ -146,7 +203,7 @@ class Profile:
             file_type: Type of the save file, only support 'txt', 'json', 'csv'.
 
         """
-        self._calculate_average_time()
+        self._calculate_and_format()
         writers = {"txt": self._save_to_txt, "json": self._save_to_json, "csv": self._save_to_csv}
         writers[file_type](path)
 
@@ -162,12 +219,15 @@ class Profile:
             self._summary = self._manager.dict()
         else:
             self._summary = {}
-        Client.do = self._statistical(self.do_function)  # type: ignore[assignment]
+        setattr(Client, "do", self._statistical(self.do_function))
+        setattr(RemoteFileMixin, "_urlopen", self._statistical_open(self.urlopen))
 
     def stop(self) -> None:
         """Stop statistical record."""
-        Client.do = self.do_function  # type: ignore[assignment]
+        setattr(Client, "do", self.do_function)
+        setattr(RemoteFileMixin, "_urlopen", self.urlopen)
         if hasattr(self, "_manager"):
+            self._summary = self._summary.copy()
             self._manager.shutdown()
             delattr(self, "_manager")
 
