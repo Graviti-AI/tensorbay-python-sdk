56194ae0a9ac8b67157ae1098281c959d562680c
yexuan.li <yexuan.li@graviti.com>
2021-11-22 20:07:11 +0800

    refactor(opendataset): return Any in mocker to avoid pyright error
    The error message:
    ```
    [Pyright reportOptionalSubscript] [E] Object of type "None" is not subscriptable
    ```
    
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/1120

--- tensorbay/opendataset/_utility/mocker.py
+++ tensorbay/opendataset/_utility/mocker.py
@@ -4,20 +4,22 @@
 #
 # pylint: disable=missing-module-docstring, invalid-name
 
+from typing import Any
+
 from tensorbay.exception import ModuleImportError
 
 
 class ImageMocker:
     """Raise import PIL error for data loader."""
 
-    def __getattribute__(self, name: str) -> None:
+    def __getattribute__(self, name: str) -> Any:
         raise ModuleImportError(module_name="pillow")
 
 
 class xmltodictMocker:
     """Raise import xmltodict error for data loader."""
 
-    def __getattribute__(self, name: str) -> None:
+    def __getattribute__(self, name: str) -> Any:
         raise ModuleImportError(module_name="xmltodict")
 
 
