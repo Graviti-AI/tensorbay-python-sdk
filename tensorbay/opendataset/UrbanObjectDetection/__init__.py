6d0f6d9695c4ee6ea7882311d96b29c4a31a9b7b
wangyuqing0424 <yuqing.wang@graviti.com>
2021-11-23 14:44:03 +0800

    feat(opendataset): add dataloader of UrbanObjectDetection dataset
    PR Closed: https://github.com/Graviti-AI/tensorbay-python-sdk/pull/1172

---
+++ tensorbay/opendataset/UrbanObjectDetection/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name
+
+"""Dataloader of UrbanObjectDetection."""
+
+from tensorbay.opendataset.UrbanObjectDetection.loader import UrbanObjectDetection
+
+__all__ = ["UrbanObjectDetection"]
